d7b1e9c42b30e82cb4086c05c821db7c607643a6
SteveHoneyman <stevehoneyman11@Steves-MacBook-Pro-2.local>
2019-12-27 16:33:06 +0000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+    //console.log('this script is from script.js..');
+    //$('h1').css({'font-family':'courier', 'font-weight':'200', 'color':'red'});
+    //console.log('if hello world is in courier, jQuery is running..');
+}); // end doc ready
